dbde51cd839c233a4230a9f7d51a818ed7b094f2
KOLAR Pierre-Eduard <s254145@dtu.dk>
2026-04-25 23:24:39 +0200

    Task 10 - Job for nsys profiling

--- scripts/GPU_job2_atol.sh
+++ scripts/GPU_job2_atol.sh
@@ -40,4 +40,4 @@ echo "Validation passed. Running GPU simulation..."
 
 MODULE_NAME=$(basename "$CANDIDATE_MODULE" .py)
 python "$CANDIDATE_MODULE" "$N_FLOORPLANS" "$DATA_DIR" \
-  > "$CSV_OUTPUT_DIR/gpu_results__${MODULE_NAME}_${N_FLOORPLANS}.csv"
+  > "$CSV_OUTPUT_DIR/gpu_results_${MODULE_NAME}_${N_FLOORPLANS}.csv"
---
+++ scripts/GPU_nsys_job.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#BSUB -J nsys_cupy
+#BSUB -q c02613
+#BSUB -W 15
+#BSUB -n 4
+#BSUB -R "span[hosts=1]"
+#BSUB -R "rusage[mem=12GB]"
+#BSUB -gpu "num=1:mode=exclusive_process"
+#BSUB -R "select[gpu80gb]"
+#BSUB -o job_outputs/%J.out
+#BSUB -e job_outputs/%J.err
+
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613_2026
+
+DATA_DIR="/dtu/projects/02613_2025/data/modified_swiss_dwellings/"
+N_FLOORPLANS=2 # reduce number of floorplans for profiling to keep runtime fast
+
+nsys profile --stats=true -o cupy_profile --force-overwrite true \
+    python src/simulate_cupy.py "$N_FLOORPLANS" "$DATA_DIR"
