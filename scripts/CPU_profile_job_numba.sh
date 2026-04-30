407d2ab93c6fc73f8da4364ccd073da61dea2c7f
Loïc Laisney <loic.laisney@proton.me>
2026-04-30 14:23:56 +0200

    new job

--- scripts/CPU_profile_job_numba.sh
+++ scripts/CPU_profile_job_numba.sh
@@ -12,29 +12,21 @@
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
 
+export PYTHONUNBUFFERED=1
 
 PROJECT_ROOT="$PWD"
 DATA_DIR="/dtu/projects/02613_2025/data/modified_swiss_dwellings/"
-PROFILE_OUTPUT_DIR="$PROJECT_ROOT/outputs/profiling"
 PROFILE_IDS_FILE="$PROJECT_ROOT/description/profile_subset_ids.txt"
-MAX_ITER=20000
-ATOL=1e-4
-SOLVER_MODULE="${1:-src/simulate_numba_cpu.py}" #Just example, can be overwritten with args
-SOLVER_FUNCTION="${2:-jacobi}" 
-SOLVER_TARGET="$(basename "$SOLVER_MODULE" .py)"
-PROFILE_TAG="$(basename "$SOLVER_MODULE" .py)_${SOLVER_FUNCTION}_profile_subset"
-mkdir -p "$PROFILE_OUTPUT_DIR"
 
+SOLVER_MODULE="${1:-src/simulate_numba_cpu.py}"
+SOLVER_FUNCTION="${2:-jacobi}"
 
-python -m kernprof -l -v \
-  -s "$PROJECT_ROOT/scripts/kernprof_setup.py" \
-  -p "$SOLVER_TARGET" \
-  -o "$PROFILE_OUTPUT_DIR/${PROFILE_TAG}.lprof" \
-  "$PROJECT_ROOT/scripts/profile_solver_numba.py" \
+echo "[INFO] Running Jacobi timing only"
+
+python -u "$PROJECT_ROOT/scripts/profile_solver_numba.py" \
   --data-dir "$DATA_DIR" \
   --ids-file "$PROFILE_IDS_FILE" \
   --solver-module "$SOLVER_MODULE" \
   --solver-function "$SOLVER_FUNCTION" \
-  --max-iter "$MAX_ITER" \
-  --atol "$ATOL" | tee "$PROFILE_OUTPUT_DIR/${PROFILE_TAG}.txt"
-
+  --max-iter 20000 \
+  --atol 1e-4
