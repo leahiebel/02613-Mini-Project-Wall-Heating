d77b16f84a06bdfa143360071664e1fdd5226f64
KOLAR Pierre-Eduard <s254145@dtu.dk>
2026-05-03 11:43:58 +0200

    Corrected GPU job for the whole dataset

--- scripts/GPU_nsys_job.sh
+++ scripts/GPU_nsys_job.sh
@@ -6,9 +6,10 @@
 #BSUB -R "span[hosts=1]"
 #BSUB -R "rusage[mem=12GB]"
 #BSUB -gpu "num=1:mode=exclusive_process"
-#BSUB -R "select[gpu80gb]"
+#BSUB -R "rusage[mem=32GB]"
 #BSUB -o job_outputs/nsys_cupy_%J.out
 #BSUB -e job_outputs/nsys_cupy_%J.err
+
 
 source /dtu/projects/02613_2025/conda/conda_init.sh
 conda activate 02613_2026
